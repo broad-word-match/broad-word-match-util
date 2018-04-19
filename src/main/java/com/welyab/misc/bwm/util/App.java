a944fa9089e14af74c964a1e8c1150ef140abc2c
Welyab Paula <welyab.exe@gmail.com>
2018-04-19 08:54:25 -0300

    initial commit

---
+++ src/main/java/com/welyab/misc/bwm/util/App.java
@@ -0,0 +1,13 @@
+package com.welyab.misc.bwm.util;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
